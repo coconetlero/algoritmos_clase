db4efc1dc1d6fd8c0ccd55366a4257ca3eda7236
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2016-02-22 03:51:11 -0600

    Add pract 2 ejer

---
+++ Prac02/Practica02_ejer.cxx
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+Escribir un programa que realice el comportamiento de la función “strcat” , el alumno deberá de implementar su propia función que realice  la concatenación de dos cadenas de caracteres  que recibe como parámetro  y entrega como resultado un apuntador a la nueva cadena.
+
+char * concatena(char *cdn1, char * cdn2);
+
+Consideraciones:
+Usar malloc para crear la tercer arreglo
+Las cadenas uno y dos no deben ser destruidas
+
+*/
+
+char * strcat(char *str_a, char * str_b);
+
+int main () 
+{
+    char *str01 = "Clase de ";
+    char *str02 = "Algoritmos y Estructuras de datos";
+    
+    char *output = strcat(str01, str02); 
+    
+    free(output);
+}
+
+char * strcat(char *str_a, char * str_b) 
+{
+    int s1 = strlen(str_a);;
+    int s2 = strlen(str_b);
+        
+    printf("Cadena 01 = %s %d \n", str_a, s1);
+    printf("Cadena 02 = %s %d \n", str_b, s2);
+    
+    /**
+    * Aqui su codigo
+    */
+    
+    char *out = (char*)malloc(sizeof(char) * (s1 +s2));
+
+
+    
+
+    return out;
+}
