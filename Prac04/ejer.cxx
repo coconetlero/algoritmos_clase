82f02769d87a481af3963fcaebf6a72e50d2017b
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2016-03-07 03:39:19 -0600

    practica 4 ejercicio

---
+++ Prac04/ejer.cxx
@@ -0,0 +1,50 @@
+/**
+ * Hacer un programa que pida un número variable de flotantes y que imprima los
+ * numeros pedidos y el cuadrado de cada uno de los numeros pedidos.
+ * Los numeros dados se deben de guardar en localidades de memoria pedida 
+ * dinámicamente. Los cuadrados de los numeros dados tambien se deben de guardar
+ * en localidades de memoria pedida dinamicamente
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main ()
+{
+    int size;
+    
+    printf("¿Cuantos elementos tiene el conjunto?\n");
+    scanf("%d",&size);
+    
+    float *array;
+    float *square_array;
+    
+    int cont;
+    for (cont=0 ; cont < num ; cont++)
+    {
+        printf("Inserte el elemento %d del conjunto.\n",cont+1);
+        // asignar el valor dado en la localidad de memoria pedida
+    }
+    
+    printf("Vector insertado:\n\t[");
+    for (cont=0 ; cont < num ; cont++)
+    { 
+        // imprimir cada uno de los valores guardados
+    }
+    printf("\t]\n");
+    
+    
+    
+    for (cont=0 ; cont < num ; cont++)
+    { 
+        // elevar al cuadrado y guradar el nuevo elemento en la localidad de memoria dada
+    }
+    
+    
+    // impirmir los elementos de las localidades de memoria asignados para los 
+    // valores elevados al cuadrado
+    
+    
+    return 0;
+}
