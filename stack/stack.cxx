fe6564c0d9f8bca633ac6d33005d7e2905534b59
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2016-04-25 09:47:30 -0500

    add create and destroy stack functions

--- stack/stack.cxx
+++ stack/stack.cxx
@@ -1,43 +1,64 @@
 #include <stack.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-
-     
-
-// ----------------------------------------------------------------------------        
-bool is_full(Stack *stack)
+// ----------------------------------------------------------------------------      
+Stack * create_stack(int size)
 {
-    // si el último elemento esta la ultima posición del array
-    if (stack->top == stack->max_size)
-        return true;
-    else
-        return false;        
-}        
+    int * array = (int *) malloc(sizeof(int) * size); 
+    assert(array != NULL);
+
+    Stack *stack = new Stack;
+    stack->array = array;
+    stack->top = -1;
+    stack->max_size = size;
+    
+    return stack;
+}
+     
+// ---------------------------------------------------------------------------- 
+void destroy_stack(Stack *stack)
+{
+    stack->top = NULL;
+    stack->max_size = NULL;
+    free(stack->array);    
+}
 
 // ----------------------------------------------------------------------------
 void push(Stack *stack, int element)
 {
-    if(is_full(stack)) 
+    // if the stack is full, then grow it to the double 
+    if (stack->top == (stack->max_size - 1))
     { 
         stack->max_size *= 2; 
         stack->array = (int*)realloc(stack->array, sizeof(int) * stack->max_size);
+        stack->top++;
+        stack->array[++stack->top] = element;  
+
     } 
     else 
     {        
-        stack->array[stack->top++] = element;    
+        stack->top++;
+        stack->array[stack->top] = element;    
     }
 }
 
 // ----------------------------------------------------------------------------
 int pop(Stack *stack)
 {
-    if (stack->top != 0)
+    // if the stack is not empty, then return the value at the top
+    if (stack->top >= 0)
     {
+        int data = stack->array[stack->top];
         stack->top--;
-        return stack->array[stack->top];
+        return data;
     }
-    return NULL;
+    else {
+        printf("Error the stack is empty \n");
+        return NULL;
+    }
+    
 }
 
 // ----------------------------------------------------------------------------
--- stack/stack.h
+++ stack/stack.h
@@ -5,18 +5,28 @@ struct Stack {
     int max_size;
 };   
 
-/**
- */
-bool is_full(Stack *stack);
 
 /**
+ * Create an instace of a Stack and points to it.
+ */
+Stack * create_stack(int size);
+
+/**
+ * Frees the memory occupied by the stack
+ */
+void destroy_stack(Stack *stack);
+
+/**
+ * Add one element to the top of the stack
  */    
 void push(Stack *stack, int element);
 
 /**
+ * Return and remove the element at the top of the stack
  */
 int pop(Stack *stack);
 
 /**
+ * Print the given stack
  */
 void print_stack(Stack *stack);
