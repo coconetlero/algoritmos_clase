e1ce31091027b28a64dfa2fb87f45df1ef92faa4
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2016-05-09 04:17:29 -0500

    pract 12

--- Prac12/Graph.py
+++ Prac12/Graph.py
@@ -1,60 +1,35 @@
-class Vertex:
-    def __init__(self, node):
-        self.id = node
+class Vertex(object):
+
+    def __init__(self, name):
+        self.name = name
         self.adjacent = {}
-
+    
     def __str__(self):
-        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
-
-    def add_neighbor(self, neighbor, weight=0):
-        self.adjacent[neighbor] = weight
-
-    def get_connections(self):
-        return self.adjacent.keys()
-
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-    def get_id(self):
-        return self.id
+        return str(self.name) + "->" + str([x.name for x in self.adjacent])
+    
+    def add_neighbor(self, vertex, edge_weight):
+        self.adjacent[vertex] = edge_weight
 
 
-class Graph:
+class Graph(object):
+    
     def __init__(self):
-        self.vert_dict = {}
-        self.num_vertices = 0
+        self.nodes = {}
 
-    def __iter__(self):
-        return iter(self.vert_dict.values())
+    def add_vertex(self, name):
+        self.nodes[name] = Vertex(name)
 
-    def add_vertex(self, node):
-        self.num_vertices = self.num_vertices + 1
-        new_vertex = Vertex(node)
-        self.vert_dict[node] = new_vertex
-        return new_vertex
-
-    def get_vertex(self, n):
-        if n in self.vert_dict:
-            return self.vert_dict[n]
-        else:
-            return None
-
-    def add_edge(self, frm, to, cost=0):
-        if frm not in self.vert_dict:
-            self.add_vertex(frm)
-        if to not in self.vert_dict:
-            self.add_vertex(to)
-
-        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
-        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
-
-    def get_vertices(self):
-        return self.vert_dict.keys()
-
+    def add_edge(self, from_node, to_node, edge_weight):
+        v1 = self.nodes[from_node]
+        v2 = self.nodes[to_node]
+        v1.add_neighbor(v2, edge_weight)
+        
+    def get_vertexes(self):
+        return nodes
+        
 if __name__ == '__main__':
 
     g = Graph()
-
     g.add_vertex('0')
     g.add_vertex('1')
     g.add_vertex('2')
@@ -80,12 +55,6 @@ if __name__ == '__main__':
     g.add_edge('5', '2', 1)
     g.add_edge('7', '2', 7)
     g.add_edge('7', '6', 5)
-
-    for v in g:
-        for w in v.get_connections():
-            vid = v.get_id()
-            wid = w.get_id()
-            print(vid, wid, v.get_weight(w))
-
-    for v in g:
-        print(v.get_id(), g.vert_dict[v.get_id()])
+    
+    print(g.nodes['0'])
+    print(g.nodes['6'])   
